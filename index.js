8f6c86cda3a801dc2f95b41bf9025d364b318fc7
meetcshah19 <meetcshah19@gmail.com>
2020-12-13 21:37:51 +0530

    Asynchronous audio file upload

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 import express from "express";
-
+import fileUpload from "express-fileupload";
+import path from "path";
 const app = express();
 const port = 6900;
+app.use(fileUpload());
+app.use(express.static('public'));
+
+app.post('/upload', function (req, res) {
+  console.log(req.files.audio);
+  if (!req.files || Object.keys(req.files).length === 0) {
+    console.log(req.files);
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let File = req.files.audio;
+
+  // Use the mv() method to place the file somewhere on your server
+  console.log(process.cwd())
+  File.mv(process.cwd()+"/"+File.name, function (err) {
+    if (err)
+      return res.status(500).send(err);
+    console.log("success");
+    res.status(400).send('File uploaded');
+  });
+});
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile('index.html');
 });
 
-// Return error on any routes that dont exist
-app.use("*", (req, res) => {
-  res.status(404).json({
-    errorMessage:
-      "Endpoint you are looking for does not exist or has been removed!",
-  });
-});
+
 const startServer = () => {
   app.listen(port, () => console.log("Listening on port 6900!"));
 };
