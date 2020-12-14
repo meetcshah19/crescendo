27fb6360573179f0ee07898d47bf0222b069b110
CapriciousRebel <code.capriciousrebel@gmail.com>
2020-12-14 17:10:24 +0530

    feat(*): add directory splitting code

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
 import express from "express";
 import fileUpload from "express-fileupload";
 import path from "path";
+import mkdirp from "mkdirp";
+import fs from "fs";
 const app = express();
 const port = 6900;
-app.use(fileUpload());
-app.use(express.static('public'));
+import cookieParser from "cookie-parser";
+import session from "express-session";
 
-app.post('/upload', function (req, res) {
+app.use(fileUpload());
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "jgfiuakyeg76341874285%^%$^#@#^!$",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.post("/upload", function (req, res) {
   console.log(req.files.audio);
   if (!req.files || Object.keys(req.files).length === 0) {
     console.log(req.files);
-    return res.status(400).send('No files were uploaded.');
+    return res.status(400).send("No files were uploaded.");
   }
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let File = req.files.audio;
 
   // Use the mv() method to place the file somewhere on your server
-  console.log(process.cwd())
-  File.mv(process.cwd()+"/"+File.name, function (err) {
-    if (err)
-      return res.status(500).send(err);
+
+  let FilePath = process.cwd() + "/uploads/" + req.sessionID + "/";
+  console.log(FilePath);
+
+  fs.mkdirSync(FilePath, { recursive: true });
+
+  File.mv(FilePath + File.name, function (err) {
+    if (err) return res.status(500).send(err);
     console.log("success");
-    res.status(400).send('File uploaded');
+    res.status(400).send("File uploaded");
   });
 });
 
 // Routes
 app.get("/", (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile("index.html");
 });
-
 
 const startServer = () => {
   app.listen(port, () => console.log("Listening on port 6900!"));
