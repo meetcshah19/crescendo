2a54a5e7e056938405df75a4a150fba9f15c93a4
CapriciousRebel <code.capriciousrebel@gmail.com>
2020-12-18 22:06:40 +0530

    solve merge conflicts for add status code

--- index.js
+++ index.js
@@ -8,54 +8,64 @@ const port = 6900;
 import cookieParser from "cookie-parser";
 import session from "express-session";
 
+var STATUS = false;
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+const splitFile = async() => {
+    await delay(10000);
+    STATUS = true;
+};
+
 app.use(fileUpload());
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(
-  session({
-    secret: "jgfiuakyeg76341874285%^%$^#@#^!$",
-    resave: false,
-    saveUninitialized: true,
-  })
+    session({
+        secret: "jgfiuakyeg76341874285%^%$^#@#^!$",
+        resave: false,
+        saveUninitialized: true,
+    })
 );
-app.get("/status", function (req, res) {
-  console.log("in status");
-  console.log(req.query["query"])
-  if (req.query["query"] == "split") {
-    res.status(200).send("split success");
-  }
+app.get("/status", function(req, res) {
+    console.log("in status");
+    console.log(req.query["query"])
+    if (req.query["query"] == "split") {
+        res.status(200).send("split success");
+    }
 });
-app.post("/upload", function (req, res) {
-  console.log(req.files.audio);
-  if (!req.files || Object.keys(req.files).length === 0) {
-    console.log(req.files);
-    return res.status(400).send("No files were uploaded.");
-  }
+app.post("/upload", function(req, res) {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send("No files were uploaded.");
+    }
 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  let File = req.files.audio;
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let File = req.files.audio;
 
-  // Use the mv() method to place the file somewhere on your server
+    // Use the mv() method to place the file somewhere on your server
 
-  let FilePath = process.cwd() + "/uploads/" + req.sessionID + "/";
-  console.log(FilePath);
+    let FilePath = process.cwd() + "/uploads/" + req.sessionID + "/";
 
-  fs.mkdirSync(FilePath, { recursive: true });
+    fs.mkdirSync(FilePath, { recursive: true });
+    File.mv(FilePath + File.name, function(err) {
+        if (err) return res.status(500).send(err);
+        res.status(200).send({ "unique-id": req.sessionID });
+    });
 
-  File.mv(FilePath + File.name, function (err) {
-    if (err) return res.status(500).send(err);
-    console.log("success");
-    res.status(200).send("File uploaded");
-  });
+    splitFile();
+});
+
+app.get("/status", function(req, res) {
+    console.log("status called!");
+    return STATUS;
 });
 
 // Routes
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+    res.sendFile("index.html");
 });
 
 const startServer = () => {
-  app.listen(port, () => console.log("Listening on port 6900!"));
+    app.listen(port, () => console.log("Listening on port 6900!"));
 };
 
 startServer();
