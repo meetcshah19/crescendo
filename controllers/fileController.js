9f9facce1e2cd9a6e799462557634b5834af98c6
CapriciousRebel <code.capriciousrebel@gmail.com>
2020-12-19 22:21:38 +0530

    add speeter code

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,4 +1,5 @@
 import fs from "fs";
+import cp from "child_process";
 
 export const upload = async (req, res) => {
   try {
@@ -7,12 +8,22 @@ export const upload = async (req, res) => {
       return res.status(400).send({ message: "Please upload a file!" });
     }
     // add the spleeter code here
-    setTimeout(() => {
-      res.status(200).send({
-        message: "Uploaded the file successfully: " + req.file.originalname,
-        client_id: req.sessionID,
-      });
-    }, 1000);
+    cp.exec(
+      "cd ../spleeterenv/bin/; ./python3 spleeter separate -i ../../cr3sc3ndo/uploads/" +
+        req.sessionID +
+        "/" +
+        req.file.originalname +
+        " -p spleeter:5stems -o ../../cr3sc3ndo/uploads/" +
+        req.sessionID,
+      "/bin/bash",
+      (data, stderr, std) => {
+        console.log(data + std + stderr);
+        res.status(200).send({
+          message: "Uploaded the file successfully: " + req.file.originalname,
+          client_id: req.sessionID,
+        });
+      }
+    );
   } catch (err) {
     res.status(500).send({
       message: `Could not upload the file: ${req.file.originalname}. ${err}`,
@@ -21,8 +32,7 @@ export const upload = async (req, res) => {
 };
 
 export const getListFiles = (req, res) => {
-  const directoryPath =
-    __basedir + "/uploads/" + req.params.client_id + "/output/";
+  const directoryPath = __basedir + "/uploads/" + req.params.client_id + "/";
   console.log(directoryPath);
 
   fs.readdir(directoryPath, function (err, files) {
@@ -38,10 +48,7 @@ export const getListFiles = (req, res) => {
       fileInfos.push({
         name: file,
         url:
-          "http://localhost:6900/uploads/" +
-          req.params.client_id +
-          "/output/" +
-          file,
+          "http://localhost:6900/uploads/" + req.params.client_id + "/" + file,
       });
     });
 
--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -9,7 +9,7 @@ import {
 const fileRouter = express.Router();
 
 fileRouter.post("/upload", uploadFileMiddleware, upload);
-fileRouter.get("/files/client_id:client_id", getListFiles);
+fileRouter.get("/files/client_id=:client_id", getListFiles);
 fileRouter.get("/files/:name", download);
 
 export default fileRouter;
