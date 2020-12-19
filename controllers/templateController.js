850db7c57cee929c91ee001fc2da2c365c5c82b4
CapriciousRebel <code.capriciousrebel@gmail.com>
2020-12-20 04:58:48 +0530

    add file write

--- controllers/templateController.js
+++ controllers/templateController.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+
 export const chooseTemplate = async (req, res) => {
   console.log(req.body);
+  let data =
+    "scale: " +
+    req.body.scale +
+    "\nwater: " +
+    req.body.water +
+    "\nparticles: " +
+    req.body.particles;
+
+  fs.writeFile(
+    __basedir + "/uploads/" + req.body.client_id + "/data.txt",
+    data,
+    (err) => {
+      if (err) return console.log(err);
+      console.log("");
+    }
+  );
 
   res.status(200).send({ Yo: "yo" });
 };
